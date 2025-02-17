9e222a85c8da54846204e051e20ab3164cd669bf
ramesh-kashyap <rameshkashyap8801@gmail.com>
2025-02-17 12:21:51 +0530

    done

--- src/models/Investment.js
+++ src/models/Investment.js
@@ -6,6 +6,10 @@ const Investment = sequelize.define('Investment', {
     user_id_fk: { type: DataTypes.INTEGER, allowNull: false },
     amount: { type: DataTypes.FLOAT, allowNull: false },
     status: { type: DataTypes.ENUM('Active', 'Inactive'), defaultValue: 'Inactive' },
+    created_at: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
 }, {
     tableName: 'investments',
     timestamps: false
